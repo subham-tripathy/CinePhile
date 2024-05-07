c73eb609e59418bede1c475faf8aeaa6248f6c58
Subham Tripathy <subhamtripathy962@gmail.com>
2024-05-07 19:55:15 +0530

    Refactor API URL and API key in search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const { query } = req.query;
-    fetch(process.env.mainURL + "search/movie?&api_key=" + process.env.api_key + "&query=" + query).then(response => response.json()).then(data => {
+    fetch(process.env.API_URL + "search/movie?&api_key=" + process.env.API_KEY + "&query=" + query).then(response => response.json()).then(data => {
         res.status(200).json(data);
     })
 }
